c1d93e543859826328de71816227b151313377af
paet-jantima <101913360+paet-jantima@users.noreply.github.com>
2023-12-28 17:21:53 +0700

    11

--- front-end/src/app/pages/mytimereport/mytimereport.component.ts
+++ front-end/src/app/pages/mytimereport/mytimereport.component.ts
@@ -71,10 +71,10 @@ export class MytimereportComponent implements OnInit, AfterViewInit {
     data.forEach((item) => {
       const processedItem: ProcessedTimeData = {
         ...item,
-        checkinDate: item.checkin ? new Date(item.checkin).toLocaleDateString() : '',
-        checkinTime: item.checkin ? new Date(item.checkin).toLocaleTimeString() : '',
-        checkoutDate: item.checkout ? new Date(item.checkout).toLocaleDateString() : '',
-        checkoutTime: item.checkout ? new Date(item.checkout).toLocaleTimeString() : '',
+        checkinDate: item.checkin ? item.checkin : '',
+        checkinTime: item.checkin ? item.checkin : '',
+        checkoutDate: item.checkout ? item.checkout : '',
+        checkoutTime: item.checkout ? item.checkout : '',
       };
 
       processedData.push(processedItem);
