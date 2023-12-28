d5d51129235f0cbfcd58dc1d02d7ddf302249370
paet-jantima <101913360+paet-jantima@users.noreply.github.com>
2023-12-28 16:14:12 +0700

    url ผิด

--- front-end/src/app/api.urls.ts
+++ front-end/src/app/api.urls.ts
@@ -1,5 +1,5 @@
 export const apiUrls = {
   authServiceApi: 'https://line-checkin.onrender.com/api/auth/',
   userServiceApi: 'https://line-checkin.onrender.com/api',
-  timeServiceApi: 'https://line-checkin.onrender.com/time/',
+  timeServiceApi: 'https://line-checkin.onrender.com/api/time/',
 }
