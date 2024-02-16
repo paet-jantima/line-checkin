817012439612d7f062c667cd580ad4452f6a7aa1
paet-jantima <101913360+paet-jantima@users.noreply.github.com>
2024-02-16 12:54:10 +0700

    121

--- front-end/src/app/services/user/user.service.ts
+++ front-end/src/app/services/user/user.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { apiUrls } from 'src/app/api.urls';
-
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +10,22 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getAllUsers(): Observable<any> {
-    return this.http.get<any>(`${apiUrls.userServiceApi}/User`);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
+    return this.http.get<any>(`${apiUrls.userServiceApi}/User`, { headers });
   }
 
   getUserById(userId: any): Observable<any> {
-    return this.http.get<any>(`${apiUrls.userServiceApi}/User/${userId}`);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
+    return this.http.get<any>(`${apiUrls.userServiceApi}/User/${userId}`, { headers });
   }
 
-
   updateById(userId: any, userData: any): Observable<any> {
-    return this.http.put<any>(`${apiUrls.userServiceApi}/User/update/${userId}`, userData);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
+    return this.http.put<any>(`${apiUrls.userServiceApi}/User/update/${userId}`, userData, { headers });
   }
 
   delete(userId: any): Observable<any> {
-    return this.http.delete<any>(`${apiUrls.userServiceApi}/User/delete/`,userId);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
+    return this.http.delete<any>(`${apiUrls.userServiceApi}/User/delete/${userId}`, { headers });
   }
-
-
-
-
 }
